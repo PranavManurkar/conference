71424a1bb3e04cf2790632efc930b65f98cef8d6
PranavManurkar <pranavmanurkar94@gmail.com>
2026-04-22 13:40:48 +0530

    workshop backend fixed

---
+++ django_backend/core/migrations/0006_merge_0005_registration_oral_presentation_and_more_0005_workshopregistration.py
@@ -0,0 +1,12 @@
+# Generated to merge the two 0005 migration branches.
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0005_registration_oral_presentation_and_more"),
+        ("core", "0005_workshopregistration"),
+    ]
+
+    operations = []
